19a7176f454b37423fb602930009be1e5a954c3e
Alkush Pipania <133737222+Alkush-Pipania@users.noreply.github.com>
2024-06-10 02:30:29 +0530

    Add files via upload

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -4,14 +4,28 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
-  authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_REACT_APP_APPID
+  apiKey: "AIzaSyCyUds5tlPdzK6j8FKIsGCgYK4SFrfVcpA",
+  authDomain: "resume-builder-2024-3d12f.firebaseapp.com",
+  projectId: "resume-builder-2024-3d12f",
+  storageBucket: "resume-builder-2024-3d12f.appspot.com",
+  messagingSenderId: "621825203040",
+  appId: "1:621825203040:web:b976d1ac35dc249a185c05"
+  // apiKey: process.env.REACT_APP_API_KEY,
+  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_APPID,
 };
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+//   authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_REACT_APP_APPID
+// };
 
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
@@ -19,4 +33,4 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 const storage = getStorage(app);
-export { auth, db, storage };
+export { auth, db ,storage };
